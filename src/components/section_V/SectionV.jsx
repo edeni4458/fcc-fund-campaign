8f3b4c2aeb87a5bd1ee186e778814bc1569190ef
Emmanuel <edeni4458@gmail.com>
2024-03-18 17:18:31 -0400

    working on new Building slider

--- src/components/section_V/SectionV.jsx
+++ src/components/section_V/SectionV.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 import '../section_V/sectionV.css'
+import ImageSlider from './ImageSlider'
+
+const slides = [
+    { url: "https://mcusercontent.com/896f3c9eb24d187e42f9531bc/images/adec791f-b28e-8e6b-19ba-024663664f11.jpeg" },
+    { url: "https://mcusercontent.com/896f3c9eb24d187e42f9531bc/_compresseds/86ec99c9-6686-9902-e530-c8ba3cab76c2.jpeg" },
+    { url: "https://mcusercontent.com/896f3c9eb24d187e42f9531bc/_compresseds/d1e52b9b-33ba-4959-c2b0-da66f514c010.jpeg" },
+    { url: "https://mcusercontent.com/896f3c9eb24d187e42f9531bc/_compresseds/a2bb4263-7028-26f9-2431-c3e466e0a169.jpeg" },
+    { url: "https://mcusercontent.com/896f3c9eb24d187e42f9531bc/_compresseds/467cd56e-2b99-22ee-bf54-7dcbfc7ddc67.jpeg" }
+];
+
+const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+}
 
 const SectionV = () => {
     return (
@@ -15,21 +30,9 @@ const SectionV = () => {
                             <li>Seats over 300</li>
                             <li>Limited Parking</li>
                         </ul>
-                        <img src='' alt='main-img' />
                     </div>
-                    <div className='building_images'>
-                        <div>
-                            <img src='' alt='img 1' />
-                        </div>
-                        <div>
-                            <img src='' alt='img 2' />
-                        </div>
-                        <div>
-                            <img src='' alt='img 3' />
-                        </div>
-                        <div>
-                            <img src='' alt='img 4' />
-                        </div>
+                    <div style={containerStyles}>
+                        <ImageSlider slides={slides}/>
                     </div>
                 </div>
             </section>
