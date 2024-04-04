48d046817e213cdee2fe5dd37b2b0a6d1f6edad7
Emmanuel <edeni4458@gmail.com>
2024-04-04 18:19:34 -0400

    mobile changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import fccLogo from '../z_images/fcc_logo-2.png'
-import '../header/header.css'
+import headerSheet from '../header/header.css'
 import EmailJs from './EmailJs'
 
 
 
 const Header = () => {
+    const myHeaderRef = useRef();
+    const [subHeader, setSubHeader] = useState()
+    console.log('subHeader', subHeader);
 
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            const entry = entries[0];
+            setSubHeader(entry.isIntersecting)
+        })
+        observer.observe(myHeaderRef.current);
+    }, [])
     return (
-        <header className='header_main-container'>
-            <div className='header_sub-container'>
+        <header  className='header_main-container'>
+            <div ref={myHeaderRef} className='header_sub-container' >
                 <div className='header_container-elements'>
                     <img src={fccLogo} alt='Fordham-Community-Church-Logo' />
                     <h2>Help us to own some brick in the Bronx in order to help reshape the spiritual climate of the Bronx through planting gospel centered churches</h2>
