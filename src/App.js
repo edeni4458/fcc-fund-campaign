e767cb4309f60995b5e442eadc30b385f0d9bc75
Emmanuel <edeni4458@gmail.com>
2024-12-13 09:47:19 -0500

    loading screen added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import { Header, SectionIII, SectionIV, SectionII, Zfooter } from './components'
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import { Route, Routes } from 'react-router';
+import LoadingScreen from './components/loading_screen/LoadingScreen';
+import { useEffect, useState } from 'react';
 
 
 
 
 function App() {
-
+const [isLoading, setIsLoading] = useState(true);
+useEffect(() => {
+  setTimeout(() => {
+    setIsLoading(false);
+  },2000)
+})
 
   return (
     <div className="App">
+      {isLoading && <LoadingScreen/>}
         
       <Routes>
         <Route path="/" element={<div><Header /><SectionIII /><SectionIV /> <div><Zfooter/></div> </div>}></Route>
---
+++ src/components/loading_screen/LoadingScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../loading_screen/loading-screen.css'
+// import loadFcc from '../z_images/fcc_logo-2.png'
+
+const LoadingScreen = () => {
+    return (
+        <div className="loading-screen">
+            <div className="loader">
+                {/* <img src={loadFcc} alt='FCC Logo'/> */}
+            </div>
+        </div>
+    );
+};
+
+export default LoadingScreen;
